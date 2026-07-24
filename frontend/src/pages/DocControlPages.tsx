8552b0e38c1500655d77cf967c238baf8173c006
periljames <jamesmuisyo99@gmail.com>
2026-07-24 09:49:42 -0400

    feat(document-control): replace legacy seeded pages with unified live workspaces

--- frontend/src/pages/DocControlPages.tsx
+++ frontend/src/pages/DocControlPages.tsx
@@ -1,23 +1,33 @@
-export {
-  DocControlDashboardPage,
-  DocControlLibraryPage,
-  DocControlDocumentDetailPage,
-} from "./DocControlPublicationsBridge";
+import { Navigate, useLocation, useParams } from "react-router-dom";
+
+export { default as DocControlDashboardPage } from "./documentControl/DocumentControlDashboardPage";
+export { default as DocControlLibraryPage } from "./documentControl/DocumentControlLibraryPage";
+export { default as DocControlDocumentDetailPage } from "./documentControl/DocumentControlRecordPage";
 
 export {
-  LegacyDocControlRedirectPage,
-  DocControlDraftsPage,
-  DocControlDraftDetailPage,
-  DocControlChangeProposalPage,
-  DocControlChangeProposalDetailPage,
-  DocControlRevisionsPage,
-  DocControlLEPPage,
-  DocControlTRPage,
-  DocControlTRDetailPage,
-  DocControlDistributionPage,
-  DocControlDistributionDetailPage,
-  DocControlArchivePage,
-  DocControlReviewsPage,
-  DocControlRegistersPage,
-  DocControlSettingsPage,
-} from "./DocControlPages.legacy";
+  DocumentControlArchivePage as DocControlArchivePage,
+  DocumentControlAuthorityPage,
+  DocumentControlChangeRequestDetailPage as DocControlChangeProposalDetailPage,
+  DocumentControlChangeRequestsPage as DocControlChangeProposalPage,
+  DocumentControlCopiesPage,
+  DocumentControlDistributionDetailPage as DocControlDistributionDetailPage,
+  DocumentControlDistributionPage as DocControlDistributionPage,
+  DocumentControlExternalSourcesPage,
+  DocumentControlIntegrationsPage,
+  DocumentControlLEPPage as DocControlLEPPage,
+  DocumentControlRegistersPage as DocControlRegistersPage,
+  DocumentControlReviewPage as DocControlReviewsPage,
+  DocumentControlRevisionPackagePage as DocControlRevisionsPage,
+  DocumentControlSettingsPage as DocControlSettingsPage,
+  DocumentControlTemporaryRevisionDetailPage as DocControlTRDetailPage,
+  DocumentControlTemporaryRevisionPage as DocControlTRPage,
+  DocumentControlWorkflowDetailPage as DocControlDraftDetailPage,
+  DocumentControlWorkflowPage as DocControlDraftsPage,
+} from "./documentControl/DocumentControlWorklistPages";
+
+export function LegacyDocControlRedirectPage() {
+  const location = useLocation();
+  const { amoCode = "" } = useParams<{ amoCode?: string }>();
+  const suffix = location.pathname.replace(/^\/doc-control/, "");
+  return <Navigate to={`/maintenance/${amoCode}/document-control${suffix}${location.search}${location.hash}`} replace />;
+}
