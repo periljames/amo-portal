755bd0959986efd01faabc8e9111f7dbccea451f
periljames <jamesmuisyo99@gmail.com>
2026-07-24 06:03:54 -0400

    Keep CAR invite CSS route-scoped

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -23,7 +23,6 @@ import "./styles/tokens.css";
 import "./styles/base.css";
 import "./styles/global.css";
 import "./styles/qms.css";
-import "./styles/car-invite-responsive.css";
 import "./styles/components/app-shell.css";
 import "./styles/components/page-header.css";
 import "./styles/components/section-card.css";
