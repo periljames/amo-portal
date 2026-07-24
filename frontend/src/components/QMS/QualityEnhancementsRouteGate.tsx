7c3b23d5e747cd1199b3e2dfa7d882c5459c8522
periljames <jamesmuisyo99@gmail.com>
2026-07-24 06:22:08 -0400

    Move Quality route gate out of the portal entrypoint

---
+++ frontend/src/components/QMS/QualityEnhancementsRouteGate.tsx
@@ -0,0 +1,20 @@
+import React, { Suspense, lazy } from "react";
+import { useLocation } from "react-router-dom";
+
+const QualityEnhancementsHost = lazy(
+  () => import("./QualityEnhancementsHost"),
+);
+
+const QualityEnhancementsRouteGate: React.FC = () => {
+  const location = useLocation();
+  const relevant = /^\/car-invite\/?$/i.test(location.pathname)
+    || /^\/maintenance\/[^/]+\/quality\/audits\/[^/]+/i.test(location.pathname);
+  if (!relevant) return null;
+  return (
+    <Suspense fallback={null}>
+      <QualityEnhancementsHost />
+    </Suspense>
+  );
+};
+
+export default QualityEnhancementsRouteGate;
