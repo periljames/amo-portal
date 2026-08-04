91f2514c1385518637bf38cf5a841f5026eab78b
periljames <jamesmuisyo99@gmail.com>
2026-08-04 05:00:21 -0400

    Load setup workflow theme bindings

--- frontend/src/pages/AdminSetupCentrePage.tsx
+++ frontend/src/pages/AdminSetupCentrePage.tsx
@@ -1,4 +1,5 @@
 import "../styles/admin-setup-location.css";
 import "../styles/admin-setup-workflow.css";
+import "../styles/admin-setup-workflow-theme.css";
 
 export { default } from "./AdminSetupCentreV2Page";
