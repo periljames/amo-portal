82a168508bf93040a5d5235baab1ee5c8123d1bd
periljames <jamesmuisyo99@gmail.com>
2026-08-05 01:12:42 -0400

    Mount complete induction read and write API

--- backend/amodb/apps/aircraft_induction/__init__.py
+++ backend/amodb/apps/aircraft_induction/__init__.py
@@ -2,5 +2,8 @@
 
 from . import bootstrap as _bootstrap  # noqa: F401
 from .router import router
+from .read_router import router as read_router
+
+router.include_router(read_router)
 
 __all__ = ["router"]
