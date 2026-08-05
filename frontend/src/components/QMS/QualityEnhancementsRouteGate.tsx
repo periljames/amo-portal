4b9a38649958bbcf115c372d4ddfa33d0ac7dab9
periljames <jamesmuisyo99@gmail.com>
2026-08-05 12:30:34 -0400

    Restrict Quality enhancements to QMS routes

--- frontend/src/components/QMS/QualityEnhancementsRouteGate.tsx
+++ frontend/src/components/QMS/QualityEnhancementsRouteGate.tsx
@@ -8,8 +8,8 @@ const QualityEnhancementsHost = lazy(
 const QualityEnhancementsRouteGate: React.FC = () => {
   const location = useLocation();
   const relevant = /^\/car-invite\/?$/i.test(location.pathname)
-    || /^\/maintenance\/[^/]+(?:\/|$)/i.test(location.pathname)
-    || /^\/platform(?:\/|$)/i.test(location.pathname);
+    || /^\/maintenance\/[^/]+\/(?:quality|qms)(?:\/|$)/i.test(location.pathname)
+    || /^\/maintenance\/[^/]+\/[^/]+\/qms(?:\/|$)/i.test(location.pathname);
   if (!relevant) return null;
   return (
     <Suspense fallback={null}>
