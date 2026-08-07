06314f95d33eaebc1938dabfb3565c95312e4e72
periljames <jamesmuisyo99@gmail.com>
2026-08-07 05:36:23 -0400

    install fail-closed OEM workbook formula controls

--- backend/amodb/apps/aircraft_architecture/content_packs/__init__.py
+++ backend/amodb/apps/aircraft_architecture/content_packs/__init__.py
@@ -6,9 +6,11 @@
 from . import services as services
 from . import governance as governance
 from . import backend_hardening as backend_hardening
+from . import backend_ingestion_hardening as backend_ingestion_hardening
 
 governance.install(services)
 backend_hardening.install(governance, services)
+backend_ingestion_hardening.install()
 
 from .router import router
 
