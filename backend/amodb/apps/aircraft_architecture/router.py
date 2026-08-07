e3004dda51b71686f3cd8c89890d3c77045af9aa
periljames <jamesmuisyo99@gmail.com>
2026-08-07 05:50:24 -0400

    mount complete OEM baseline assembly workflow

--- backend/amodb/apps/aircraft_architecture/router.py
+++ backend/amodb/apps/aircraft_architecture/router.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter
 from .aircraft_catalogue.router import router as catalogue_router
 from .aircraft_induction.router import router as induction_router
 from .content_packs.backend_admin_router import router as content_pack_backend_admin_router
+from .content_packs.backend_assembly_router import router as content_pack_assembly_router
 from .content_packs.backend_currentness_router import router as content_pack_currentness_router
 from .content_packs.backend_router import router as content_pack_backend_router
 from .content_packs.ingestion_router import router as content_pack_ingestion_router
@@ -25,4 +26,5 @@ router.include_router(content_pack_ingestion_router)
 router.include_router(content_pack_backend_router)
 router.include_router(content_pack_backend_admin_router)
 router.include_router(content_pack_currentness_router)
+router.include_router(content_pack_assembly_router)
 router.include_router(daily_utilisation_router)
