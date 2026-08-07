07f6d67571898946984bb7978920bd90ac4c2522
periljames <jamesmuisyo99@gmail.com>
2026-08-07 03:52:29 -0400

    Mount OEM maintenance-planning workbook preview API

--- backend/amodb/apps/aircraft_architecture/router.py
+++ backend/amodb/apps/aircraft_architecture/router.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 
 from .aircraft_catalogue.router import router as catalogue_router
 from .aircraft_induction.router import router as induction_router
+from .content_packs.ingestion_router import router as content_pack_ingestion_router
 from .content_packs.router import router as content_packs_router
 from .daily_utilisation.router import router as daily_utilisation_router
 from .effectivity.router import router as effectivity_router
@@ -17,4 +18,5 @@ router.include_router(import_staging_router)
 router.include_router(tenant_programmes_router)
 router.include_router(induction_router)
 router.include_router(content_packs_router)
+router.include_router(content_pack_ingestion_router)
 router.include_router(daily_utilisation_router)
