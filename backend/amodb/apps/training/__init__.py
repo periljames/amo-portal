61ed12f336eaba92c3f0938d7b471215a38a0fb0
periljames <jamesmuisyo99@gmail.com>
2026-08-17 23:52:06 -0400

    training: install learner workflow routes

--- backend/amodb/apps/training/__init__.py
+++ backend/amodb/apps/training/__init__.py
@@ -7,6 +7,7 @@ from . import compliance as _training_compliance
 from . import course_lifecycle as _course_lifecycle
 from . import router as _router_module
 from .learner_invitation_routes import install_training_learner_invitation_routes
+from .learner_workflow_routes import install_training_learner_workflow_routes
 from .notification_dispatch_routes import install_training_notification_dispatch_routes
 from .record_presentation import install_training_record_presentation
 from .shared_storage_policy import install_training_shared_storage
@@ -16,6 +17,7 @@ install_training_shared_storage(_router_module)
 install_training_record_presentation(_router_module)
 install_training_workflow_completion(_router_module)
 install_training_learner_invitation_routes(_router_module)
+install_training_learner_workflow_routes(_router_module)
 install_training_notification_dispatch_routes(_router_module)
 
 # Compatibility boundary for legacy compliance internals. Runtime relationship
@@ -26,6 +28,7 @@ _training_compliance.is_refresher_course = _course_lifecycle.is_recurrent_course
 
 __all__ = [
     "install_training_learner_invitation_routes",
+    "install_training_learner_workflow_routes",
     "install_training_notification_dispatch_routes",
     "install_training_record_presentation",
     "install_training_shared_storage",
